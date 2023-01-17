a874cd838d98efc78f76d0f2b119cf866e5e149a
breesebaker <cbreesebaker@gmail.com>
2023-01-17 16:26:08 -0600

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <>
-      <div className="landing-page">
+      <div className="main-page">
         <NavBar
           setSearchResults={setSearchResults}
           setFilterResults={setFilterResults}
